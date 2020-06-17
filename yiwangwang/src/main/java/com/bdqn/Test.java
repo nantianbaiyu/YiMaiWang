e8c99544a2a87eefc8f01137ead4f85e8da45539
zoulei <337544076@qq,com>
2020-06-17 23:56:22 +0800

    主綫添加

--- yiwangwang/src/main/java/com/bdqn/Test.java
+++ yiwangwang/src/main/java/com/bdqn/Test.java
@@ -2,6 +2,10 @@ package com.bdqn;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println(111);
+        System.out.println("主綫添加");
+        System.out.println("主綫添加2");
+
     }
 }
